67691b5371c294658888c47334bfe51770a57ac2
WiBertt <131600710+WiBertt@users.noreply.github.com>
2025-06-03 10:10:52 -0500

    Add files via upload

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the BAND NAME GENERATOR!!")
+petName = input("What is your childhood pet name?")
+townName = input("What town did you grow up in?")
+print("Your bands name is " + petName + townName)
